951792917cc54b742787dc7e003d0c65054d77d8
khushal-timbaliya <khushaltimbaliya@gmail.com>
2024-04-19 09:28:37 +0530

    weekly project

--- weekly project 4/jquery.js
+++ weekly project 4/jquery.js
@@ -1,1 +1,13 @@
-    
+$(document).ready(function(){
+$(window).scroll(function(){
+    if($(document).scrollTop()>100){
+        $("nav").css("background-color","#303030")
+        $("nav").css("box-shadow","rgba(0, 0, 0, 0.35) 0px 5px 15px")
+    }
+    else{
+        $("nav").css("background-color","transparent")
+        $("nav").css("box-shadow","none")
+    }
+
+})
+})
